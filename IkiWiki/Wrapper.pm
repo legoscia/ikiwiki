ce25e23e5edaea420d09d85b7e6d3fac2a07d7dc
Magnus Henoch <magnus.henoch@gmail.com>
2011-02-24 23:47:46 +0000

    * Wrapper.pm (gen_wrapper): Get TZ and LANG from environment.

--- IkiWiki/Wrapper.pm
+++ IkiWiki/Wrapper.pm
@@ -148,8 +148,9 @@ $check_commit_hook
 $envsave
 	newenviron[i++]="HOME=$ENV{HOME}";
 	newenviron[i++]="PATH=$ENV{PATH}";
+	newenviron[i++]="TZ=$ENV{TZ}";
+	newenviron[i++]="LANG=$ENV{LANG}}";
 	newenviron[i++]="WRAPPED_OPTIONS=$configstring";
-	newenviron[i++]="LC_CTYPE=en_US.utf8";
 
 #ifdef __TINYC__
 	/* old tcc versions do not support modifying environ directly */
